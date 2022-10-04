d29dbb6fea8602c97a6c7f1a4a11810d7658c8d4
Nghia N <53877485+nghian95@users.noreply.github.com>
2022-10-04 10:57:26 -0500

    Small ActivityController changes

--- src/main/java/com/nghianguyen/FitnessTracker/controller/ActivityController.java
+++ src/main/java/com/nghianguyen/FitnessTracker/controller/ActivityController.java
@@ -1,6 +1,5 @@
 package com.nghianguyen.FitnessTracker.controller;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,12 +7,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.nghianguyen.FitnessTracker.model.Activity;
 import com.nghianguyen.FitnessTracker.service.ActivityService;
@@ -44,7 +42,8 @@ public class ActivityController {
    */
    
    @PostMapping("/activity")
-   public void createActivity(@RequestBody Activity activity) {
+   public void addActivity(@ModelAttribute Activity activity, Model model) {
+//	   model.addAttribute("activity", activity);
 	   activityService.addActivity(activity);
    }
 
