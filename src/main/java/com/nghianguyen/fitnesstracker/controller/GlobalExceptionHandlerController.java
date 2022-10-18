1c9d87e2c317a3e82c0edbac8eadf41b7c0d8f88
Nghia N <53877485+nghian95@users.noreply.github.com>
2022-10-18 14:43:24 -0500

    Got the 404 error page to work properly
    Added @Controller and implemented ErrorController interface to override the handleError() method. Replaced defaultErrorHandler() with the handleError() method.

--- src/main/java/com/nghianguyen/fitnesstracker/controller/GlobalExceptionHandlerController.java
+++ src/main/java/com/nghianguyen/fitnesstracker/controller/GlobalExceptionHandlerController.java
@@ -7,19 +7,22 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.dao.DataAccessException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
-public class GlobalExceptionHandlerController {
+@Controller
+public class GlobalExceptionHandlerController implements ErrorController{
 	public static final String DEFAULT_ERROR_VIEW = "error";
 	private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandlerController.class);
 	
@@ -56,17 +59,20 @@ public class GlobalExceptionHandlerController {
 	 * Handles any exceptions that occur when the page is supposed to have more parameters but 
 	 * is missing them. i.e. Update pages that have values passed to them. 
 	 */
-	@ExceptionHandler(MissingServletRequestParameterException.class)
+	@ExceptionHandler(value = MissingServletRequestParameterException.class)
 	public String missingParameter(HttpServletRequest req, Exception ex, Model model) {
 		log.error("Request: " + req.getRequestURL() + " raised " + ex);
 		model.addAttribute("msg", "Please use the user interface to access the destination page.");
 		return "error";
 	}
 	
-	//Meant to be the default error method handler. 
-	//not working as expected for 404
+	/*
+	 * Meant to be the default error method handler for anything that hasn't been handled already. 
+	 * Includes the 404 error where there's no mapping for a specific URL. 
+	 */
 	@ExceptionHandler(value = Exception.class)
-	public String defaultErrorHandler(HttpServletRequest req, Exception ex, Model model) throws Exception {
+	@GetMapping("/error")
+	public String handleError(HttpServletRequest req, Exception ex, Model model) throws Exception {
 //		for custom errors with @ResponseStatus
 //	    if (AnnotationUtils.findAnnotation
 //                (ex.getClass(), ResponseStatus.class) != null) {
